7482cbdf987a022e1eb1fb7fad758a8b26af1d6d
Weslley Santo <weslley.santo@gmail.com>
2023-05-14 12:54:47 -0300

    FInalizando aula 05. Middleware e integração com front-end

--- routes/routes.go
+++ routes/routes.go
@@ -5,12 +5,14 @@ import (
 	"net/http"
 
 	"github.com/WeslleySanto/Go-desenvolvendo-uma-API-Rest/controllers"
+	"github.com/WeslleySanto/Go-desenvolvendo-uma-API-Rest/middleware"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeJson)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.GetAllPersonalities).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.GetPersonalitiesById).Methods("Get")
